2e122207fb6220b07b52ddf0464641fb6d231b34
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-15 14:52:44 +0300

    Refactor for using React

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore, bindActionCreators } from "redux";
+import { createStore } from "redux";
 import reducer from "./reducer";
-import * as actions from "./actions";
-import Counter from "./counter";
+import App from "./components/App";
+import { Provider } from "react-redux";
 
 const store = createStore(reducer);
 
-const { dispatch, subscribe, getState } = store;
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-const update = () => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(
-    <React.StrictMode>
-      <Counter
-        counter={getState().value}
-        inc={inc}
-        dec={dec}
-        rnd={() => {
-          const value = Math.floor(Math.random() * 10);
-          rnd(value);
-        }}
-      />
-    </React.StrictMode>
-  );
-};
-update();
-subscribe(update);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
