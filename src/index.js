d081eebd014c50499de12b3ace6dc5bd3a2bb868
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-15 14:47:37 +0300

    Use React with Redux

--- src/index.js
+++ src/index.js
@@ -3,25 +3,28 @@ import ReactDOM from "react-dom/client";
 import { createStore, bindActionCreators } from "redux";
 import reducer from "./reducer";
 import * as actions from "./actions";
+import Counter from "./counter";
 
 const store = createStore(reducer);
 
 const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById("counter").textContent = getState().value;
-};
-
-subscribe(update);
-
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
-document.getElementById("inc").addEventListener("click", inc);
-document.getElementById("dec").addEventListener("click", dec);
-document.getElementById("rnd").addEventListener("click", () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+const update = () => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <Counter
+        counter={getState().value}
+        inc={inc}
+        dec={dec}
+        rnd={() => {
+          const value = Math.floor(Math.random() * 10);
+          rnd(value);
+        }}
+      />
+    </React.StrictMode>
+  );
+};
+update();
+subscribe(update);
