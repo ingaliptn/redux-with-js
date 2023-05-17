bafd8f2b718acdb36f4e6525c7b57c2bee821fcc
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2023-05-17 14:52:36 +0300

    Finish

--- src/components/Сounter.js
+++ src/components/Сounter.js
@@ -1,46 +1,49 @@
-import { connect } from "react-redux";
-import * as actions from "../actions";
-import { Component } from "react";
+//import { connect } from "react-redux";
+import { inc, dec, rnd } from "../actions";
+import { useSelector, useDispatch } from "react-redux";
 
-// const Counter = ({ counter, inc, dec, rnd }) => {
-//   return (
-//     <div className="jumbotron">
-//       <h1>{counter}</h1>
-//       <button onClick={dec} className="btn btn-primary">
-//         DEC
-//       </button>
-//       <button onClick={inc} className="btn btn-primary">
-//         INC
-//       </button>
-//       <button onClick={rnd} className="btn btn-info">
-//         RND
-//       </button>
-//     </div>
-//   );
-// };
-class Counter extends Component {
-  render() {
-    const { counter, inc, dec, rnd } = this.props;
-    return (
-      <div className="jumbotron">
-        <h1>{counter}</h1>
-        <button onClick={dec} className="btn btn-primary">
-          DEC
-        </button>
-        <button onClick={inc} className="btn btn-primary">
-          INC
-        </button>
-        <button onClick={rnd} className="btn btn-info">
-          RND
-        </button>
-      </div>
-    );
-  }
-}
-const mapStateToProps = (state) => {
-  return {
-    counter: state.value,
-  };
+const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div style={{ textAlign: "center" }} className="jumbotron">
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">
+        DEC
+      </button>
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">
+        INC
+      </button>
+      <button onClick={() => dispatch(rnd())} className="btn btn-info">
+        RND
+      </button>
+    </div>
+  );
 };
+// class Counter extends Component {
+//   render() {
+//     const { counter, inc, dec, rnd } = this.props;
+//     return (
+//       <div className="jumbotron">
+//         <h1>{counter}</h1>
+//         <button onClick={dec} className="btn btn-primary">
+//           DEC
+//         </button>
+//         <button onClick={inc} className="btn btn-primary">
+//           INC
+//         </button>
+//         <button onClick={rnd} className="btn btn-info">
+//           RND
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.value,
+//   };
+// };
 
-export default connect(mapStateToProps, actions)(Counter);
+// export default connect(mapStateToProps, actions)(Counter);
+export default Counter;
